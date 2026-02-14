560f8ddb591a82b0aee1db2f524af4c2a896f95f
sykot-mahmud <mahamudunnobi15-8266@diu.edu.bd>
2026-02-14 11:02:31 +0600

    git scrap updated

--- scrap.py
+++ scrap.py
@@ -1,1 +1,9 @@
-print("hallo world")
+# print("hallo world")
+
+def my_function(title,*args,**kwargs):
+    print("title:",title)
+    print("positional arguments",args)
+    print("keyword argument:",kwargs)
+    
+my_function('scrapping project',"Emil", "Tobias", age = 25, city = "Oslo")
+    
