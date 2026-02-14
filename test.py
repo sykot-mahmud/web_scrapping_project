84f900ae84537db35b27b866df0b22630436f42b
sykot-mahmud <mahamudunnobi15-8266@diu.edu.bd>
2026-02-14 10:34:05 +0600

    test file

---
+++ test.py
@@ -0,0 +1,11 @@
+# # var=input("Enter a word")
+# var='computer'
+# for i in range(1,51):
+#     print(var)
+#     print("\t")
+
+for i in range(1,10):
+    for j in range(1,i+1):
+        print(j,end=' ')
+    print()
+    
