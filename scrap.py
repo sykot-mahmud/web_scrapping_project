0e3646d8be131f979ac321c0322dd26ad46154ce
sykot-mahmud <mahamudunnobi15-8266@diu.edu.bd>
2026-02-14 11:13:49 +0600

    scrap file updated

--- scrap.py
+++ scrap.py
@@ -7,3 +7,10 @@ def my_function(title,*args,**kwargs):
     
 my_function('scrapping project',"Emil", "Tobias", age = 25, city = "Oslo")
     
+    
+def my_function(a, b, c):
+  return a + b + c
+
+numbers = [1, 2, 3]
+result = my_function(*numbers) # Same as: my_function(1, 2, 3)
+print(result)
